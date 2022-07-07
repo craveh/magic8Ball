68d9caf8a27ff02769475eea7772a770878df298
cxr956 <61929297+craveh@users.noreply.github.com>
2022-07-07 01:28:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+ /* 
+    SCRIPT.JS
+
+    This is where you'll complete the SkillBuilder. Look at the Markdown tab (in the same pane as the console) for detailed README instuctions!
+*/
+
+let positiveAnswers = [
+  "As I see it, yes",
+  "It is certain",
+  "It is decidedly so",
+  "Yes",
+  "Yes, definitely"
+];
+
+let negativeAnswers = [
+  "My reply is no",
+  "My sources say no",
+  "There is no way",
+  "No",
+  "Absolutely not!"
+];
+
+let maybeAnswers = [
+  "Ask again later",
+  "Better to not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Reply hazy try again"
+];
+
+const NO_QUESTION_WARNING = "You need to ask a question!";
+
+function chooseRandomAnswerType() {
+	// Part 1: Your code here 👇
+
+}
+
+chooseRandomAnswerTypeTest();
+
+function chooseRandomAnswer(answerType) {
+	// Part 2: Your code here 👇
+
+}
+
+// chooseRandomAnswerTest();
+
+
+function onAnswerRequested() {
+  // Part 3: Your code here 👇
+	
+}
+
+// onAnswerRequestedTest();
+
+
+/* LEVEL UP - describe how it works below!
+  Describe here!
+*/
